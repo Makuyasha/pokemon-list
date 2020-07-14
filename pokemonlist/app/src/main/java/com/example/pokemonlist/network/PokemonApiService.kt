577fd07f3aa29c2136eeaf781157d3f91d409472
Marco Jacaz <marco.jacaz@external.nexi.it>
2020-07-14 14:32:46 +0200

    first get response

--- pokemonlist/app/src/main/java/com/example/pokemonlist/network/PokemonApiService.kt
+++ pokemonlist/app/src/main/java/com/example/pokemonlist/network/PokemonApiService.kt
@@ -1,3 +1,25 @@
 package com.example.pokemonlist.network
 
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
 private const val BASE_URL = "https://pokeapi.co/"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface PokemonApiService {
+    @GET("api/v2/pokemon")
+    fun getPokemonList():
+            Call<String>
+}
+
+object PokemonApi {
+    val retrofitService : PokemonApiService by lazy {
+        retrofit.create(PokemonApiService::class.java)
+    }
+}
